3941adc1a509bbf5976be2bc5492ec7d5b19799b
LeoGidev <leogimenez.dev@gmail.com>
2024-05-10 16:11:28 -0300

    estructura

--- tetris.py
+++ tetris.py
@@ -154,3 +154,16 @@ def convert_shape_format(piece):
         positions[i] = (pos[0] - 2, pos[1] - 4)
 
     return positions
+
+# Función para verificar colisión de una pieza
+def valid_space(piece, grid):
+    accepted_positions = [[(j, i) for j in range(10) if grid[i][j] == BLACK] for i in range(20)]
+    accepted_positions = [j for sub in accepted_positions for j in sub]
+
+    formatted = convert_shape_format(piece)
+
+    for pos in formatted:
+        if pos not in accepted_positions:
+            if pos[1] > -1:
+                return False
+    return True
