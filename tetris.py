2244a0a73da3bdf31ad0001f130f1cfe478fab24
LeoGidev <leogimenez.dev@gmail.com>
2024-05-10 16:17:42 -0300

    estructura

--- tetris.py
+++ tetris.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+
 # Dimensiones de la pantalla
 SCREEN_WIDTH = 300
 SCREEN_HEIGHT = 600
@@ -123,6 +124,7 @@ T = [['.....',
       '.00..',
       '..0..',
       '.....']]
+
 SHAPES = [S, Z, I, O, J, L, T]
 SHAPES_COLORS = [GREEN, RED, CYAN, YELLOW, BLUE, ORANGE, PURPLE]
 
@@ -156,17 +158,15 @@ def convert_shape_format(piece):
     return positions
 
 # Función para verificar colisión de una pieza
-def valid_space(piece, grid):
-    accepted_positions = [[(j, i) for j in range(10) if grid[i][j] == BLACK] for i in range(20)]
-    accepted_positions = [j for sub in accepted_positions for j in sub]
-
+def valid_space(piece, grid, locked):
     formatted = convert_shape_format(piece)
 
     for pos in formatted:
-        if pos not in accepted_positions:
-            if pos[1] > -1:
+        if pos[1] > -1:
+            if pos in locked:
                 return False
     return True
+
 
 # Función para limpiar las líneas completadas
 def clear_rows(grid, locked):
@@ -208,7 +208,6 @@ def draw_next_shape(shape, screen):
 
     screen.blit(label, (sx + 10, sy - 30))
 
-
 # Función para dibujar el borde del tablero
 def draw_grid(screen, grid):
     for i in range(len(grid)):
@@ -230,3 +229,100 @@ def draw_window(screen, grid):
     pygame.draw.rect(screen, RED, (TOP_LEFT_X, TOP_LEFT_Y, PLAY_WIDTH * BLOCK_SIZE, PLAY_HEIGHT * BLOCK_SIZE), 5)
 
     draw_grid(screen, grid)
+
+# Función principal del juego
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Tetris')
+
+    locked_positions = {}
+    grid = [[BLACK for _ in range(PLAY_WIDTH)] for _ in range(PLAY_HEIGHT)]
+
+    change_piece = False
+    run = True
+    current_piece = create_piece()
+    next_piece = create_piece()
+    clock = pygame.time.Clock()
+    fall_time = 0
+    fall_speed = 0.27
+    score = 0
+    lines = 0
+
+    while run:
+        grid = [[BLACK for _ in range(PLAY_WIDTH)] for _ in range(PLAY_HEIGHT)]
+        fall_time += clock.get_rawtime()
+        clock.tick()
+
+        # Aumentar la velocidad cada 20 líneas
+        if lines >= 20:
+            fall_speed -= 0.005
+            lines = 0
+
+        # Movimiento automático hacia abajo
+        if fall_time/1000 >= fall_speed:
+            fall_time = 0
+            current_piece.y += 1
+            if not(valid_space(current_piece, grid)) and current_piece.y > 0:
+                current_piece.y -= 1
+                change_piece = True
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.display.quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece.x += 1
+
+                if event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece.x -= 1
+
+                if event.key == pygame.K_DOWN:
+                    current_piece.y += 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece.y -= 1
+
+                if event.key == pygame.K_UP:
+                    current_piece.rotation += 1
+                    if not(valid_space(current_piece, grid)):
+                        current_piece.rotation -= 1
+
+        shape_pos = convert_shape_format(current_piece)
+
+        for i in range(len(shape_pos)):
+            x, y = shape_pos[i]
+            if y > -1:
+                grid[y][x] = current_piece.color
+
+        if change_piece:
+            for pos in shape_pos:
+                p = (pos[0], pos[1])
+                locked_positions[p] = current_piece.color
+            current_piece = next_piece
+            next_piece = create_piece()
+            change_piece = False
+            score += clear_rows(grid, locked_positions)
+            lines += clear_rows(grid, locked_positions)
+
+        draw_window(screen, grid)
+        draw_next_shape(next_piece, screen)
+        pygame.display.update()
+
+        # Condicional de victoria
+        if score >= 100:
+            font = pygame.font.SysFont('arial', 60)
+            label = font.render('YOU WIN!', 1, WHITE)
+            screen.blit(label, (TOP_LEFT_X + PLAY_WIDTH/2 - (label.get_width()/2), TOP_LEFT_Y + PLAY_HEIGHT/2 - (label.get_height()/2)))
+            pygame.display.update()
+            pygame.time.delay(2000)
+            run = False
+
+    pygame.display.quit()
+
+main()
